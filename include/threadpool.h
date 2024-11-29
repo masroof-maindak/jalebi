564fec90990c01559763f1b1a91997423b88ba53
masroof-maindak <mujtaba.asim.amin@gmail.com>
2024-11-30 03:33:11 +0500

    fix: internal_f wasn't deep-copying or dequeuing chore: format & cleanup

--- include/threadpool.h
+++ include/threadpool.h
@@ -7,15 +7,15 @@
 
 struct threadpool {
 	size_t size;
+	struct queue *q;
 	pthread_t *threads;
-    struct queue *q;
-	void *(*fp)(void*);
-	pthread_mutex_t lock; 
-	pthread_cond_t notify; 
+	void *(*fp)(void *);
+	pthread_mutex_t lock;
+	pthread_cond_t notify;
 };
 
+void *internal_f(void *arg);
 struct threadpool *create_threadpool(size_t n, void *(*fp)(void *));
-void* internal_f(void* arg);
 void delete_threadpool(struct threadpool *tp);
 
 #endif // THREADPOOL_H_H
--- src/threadpool.c
+++ src/threadpool.c
@@ -24,19 +24,23 @@ struct threadpool *create_threadpool(size_t n, void *(*fp)(void *)) {
 		free(tp);
 		return NULL;
 	}
+
 	tp->q = malloc(sizeof(struct queue));
 	if (tp->q == NULL) {
 		perror("malloc() in create_tp() - queue");
+		free(tp->threads);
 		free(tp);
 		return NULL;
 	}
+
 	tp->fp = fp;
-	pthread_mutex_init(&tp->lock);
+
+	pthread_mutex_init(&tp->lock, NULL);
 	pthread_cond_init(&tp->notify, NULL);
+
 	/* CHECK: what to do if failure occurs ? */
 	for (size_t i = 0; i < n; i++) {
-		if (pthread_create(&tp->threads[i], NULL, internal_f, (void *)tp) !=
-			0) {
+		if (pthread_create(&tp->threads[i], NULL, internal_f, tp) != 0) {
 			perror("pthread_create() in create_tp()");
 			return NULL;
 		}
@@ -50,47 +54,42 @@ struct threadpool *create_threadpool(size_t n, void *(*fp)(void *)) {
 	return tp;
 }
 
+/**
+ * @brief if work is available, execute it
+ */
 void *internal_f(void *arg) {
 	void *task;
-	if (!arg) {
-		perror("arg null in internal_f()");
-		return NULL;
-	}
 	struct threadpool *tp = (struct threadpool *)arg;
+
 	for (;;) {
 		pthread_mutex_lock(&tp->lock);
-		while (tp->q->size == 0) {
+		while (tp->q->size == 0)
 			pthread_cond_wait(&tp->notify, &tp->lock);
-		}
-		task = peek_top(tp->q);
+		task = copy_top(tp->q);
+		dequeue(tp->q);
 		pthread_mutex_unlock(&tp->lock);
+
 		tp->fp(task);
 	}
+
 	return NULL;
 }
 
-
-void add_task(struct threadpool* tp, void* data){
-	if(!tp){
-		perror("tp is null in add_task()");
-		return;
-
-	}
-	if(!data){
-		perror("data is null in add_task()");
-		return;
-	}
-    pthread_mutex_lock(&tp->lock);
+/**
+ * @brief add a task to the threadpool
+ */
+void add_task(struct threadpool *tp, void *data) {
+	pthread_mutex_lock(&tp->lock);
 	enqueue(tp->q, data);
 	pthread_mutex_unlock(&tp->lock);
+
 	pthread_cond_signal(&tp->notify);
 }
-
-
 
 void delete_threadpool(struct threadpool *tp) {
 	if (tp == NULL)
 		return;
+	/* TODO: destroy mutex & cond_var */
 	free(tp->threads);
 	free(tp);
 }
