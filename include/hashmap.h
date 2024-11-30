0bd83d04c50f5dd44dfe1f8449aba3eb4c36e646
murtazak9180 <=>
2024-11-30 12:33:41 +0500

    feat: hashmap to manage answers

---
+++ include/hashmap.h
@@ -0,0 +1,22 @@
+#ifndef HASHMAP_H
+#define HASHMAP_H
+
+#include <uuid/uuid.h>
+#include "answer.h"
+#include "uthash.h"
+
+struct hashmap{
+ uuid_t uuid; 
+ struct answer* answers; 
+ UT_hash_handle hh; 
+};
+
+int add_to_map(struct hashmap **map, uuid_t key, struct answer *answers);
+int delete_from_map(struct hashmap **map, uuid_t key);
+struct answer *get_answers(struct hashmap *map, uuid_t key);
+int key_exists(struct hashmap *map, uuid_t key);
+int free_map(struct hashmap **map);
+
+
+
+#endif
