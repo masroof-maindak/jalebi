825c9d2dbfb619a8bc9e0f204cb5d8f60bac30df
murtazak9180 <=>
2024-11-30 15:11:57 +0500

    Fix: removed condmap-related functions

--- include/hashmap.h
+++ include/hashmap.h
@@ -1,21 +1,27 @@
 #ifndef HASHMAP_H
 #define HASHMAP_H
 
-#include <uuid/uuid.h>
+
 
 #include "server.h"
 #include "uthash.h"
+#include <uuid/uuid.h>
 
-struct hashmap {
+struct answermap {
 	uuid_t uuid;
 	answer *answers;
 	UT_hash_handle hh;
 };
 
-int add_to_map(struct hashmap **map, uuid_t key, answer *answers);
-int delete_from_map(struct hashmap **map, uuid_t key);
-answer *get_answers(struct hashmap *map, uuid_t key);
-int key_exists(struct hashmap *map, uuid_t key);
-int free_map(struct hashmap **map);
+
+//for answermap
+int add_to_answermap(struct answermap **map, uuid_t key, answer *answers);
+answer *get_answers(struct answermap *map, uuid_t key);
+int delete_from_answermap(struct answermap **map, uuid_t key);
+int key_exists_answermap(struct answermap*map, uuid_t key);
+int free_answermap(struct answermap **map);
+
+
+
 
 #endif
--- src/hashmap.c
+++ src/hashmap.c
@@ -4,10 +4,10 @@
 
 #include "../include/hashmap.h"
 
-int add_to_map(struct hashmap **map, uuid_t key, answer *answers) {
-	struct hashmap *entry = malloc(sizeof(*entry));
+int add_to_answermap(struct answermap **map, uuid_t key, answer *answers) {
+	struct answermap *entry = malloc(sizeof(struct answermap));
 	if (entry == NULL) {
-		perror("malloc() in add_to_map()");
+		perror("malloc() in add_to_answermap()");
 		return -1;
 	}
 
@@ -17,37 +17,49 @@ int add_to_map(struct hashmap **map, uuid_t key, answer *answers) {
 	return 0;
 }
 
-int delete_from_map(struct hashmap **map, uuid_t key) {
-	struct hashmap *entry;
-
+int delete_from_answermap(struct answermap **map, uuid_t key) {
+	struct answermap *entry;
 	HASH_FIND(hh, *map, key, sizeof(uuid_t), entry);
-	if (entry == NULL)
+	if (entry == NULL) {
 		return -1;
+	}
 	HASH_DEL(*map, entry);
-
 	free(entry);
 	return 0;
 }
 
-answer *get_answers(struct hashmap *map, uuid_t key) {
-	struct hashmap *entry;
+answer *get_answers(struct answermap *map, uuid_t key) {
+	struct answermap *entry;
 	HASH_FIND(hh, map, key, sizeof(uuid_t), entry);
 	if (entry == NULL)
 		return NULL;
 	return entry->answers;
 }
 
-int key_exists(struct hashmap *map, uuid_t key) {
-	struct hashmap *entry;
+int key_exists_answermap(struct answermap *map, uuid_t key) {
+	struct answermap *entry;
 	HASH_FIND(hh, map, key, sizeof(uuid_t), entry);
 	return entry ? 1 : 0;
 }
 
-int free_map(struct hashmap **map) {
+int free_answermap(struct answermap **map) {
+	if (map == NULL || *map == NULL) {
+		return -1;
+	}
+	struct answermap *current_entry, *tmp;
+	HASH_ITER(hh, *map, current_entry, tmp) {
+		HASH_DEL(*map, current_entry);
+		free(current_entry);
+	}
+	*map = NULL;
+	return 0;
+}
+
+int free_map(struct answermap **map) {
 	if (map == NULL || *map == NULL)
 		return -1;
 
-	struct hashmap *current_entry, *tmp;
+	struct answermap *current_entry, *tmp;
 	HASH_ITER(hh, *map, current_entry, tmp) {
 		HASH_DEL(*map, current_entry);
 		free(current_entry);
